3dd6e91f333ac3d8b982c646d47d2d3fba4c2d73
danikarik <daniyar@hey.com>
2024-07-21 17:21:36 +0500

    Update protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
-  # TODO: move to authentication module
-  protect_from_forgery
+  protect_from_forgery with: :exception, unless: -> { authenticated_by.bot_key? }
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+    def set_authenticated_by(method)
+      @authenticated_by = method.to_s.inquiry
+    end
+
+    def authenticated_by
+      @authenticated_by ||= "".inquiry
+    end
 end
