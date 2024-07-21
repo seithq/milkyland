f9417ae9d537bcb5ab3fb7065a202af07903ac63
danikarik <daniyar@hey.com>
2024-07-21 17:16:55 +0500

    Add protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  # TODO: move to authentication module
+  protect_from_forgery
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
