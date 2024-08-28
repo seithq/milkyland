a56803d09d80bf63d34f37d31370415aaa9a4678
danikarik <daniyar@hey.com>
2024-08-28 18:50:00 +0500

    Add sub menu nav link

---
+++ app/javascript/controllers/sub_menu_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+import { enter, leave, toggle } from "el-transition"
+
+export default class extends Controller {
+  static targets = [ "menu", "arrow" ]
+  static classes = [ "expanded", "collapsed" ]
+  static values = { open: Boolean }
+
+  openValueChanged() {
+    if (this.openValue) {
+      this.arrowTarget.classList.add(this.expandedClass)
+      this.arrowTarget.classList.remove(this.collapsedClass)
+      enter(this.menuTarget)
+    } else {
+      this.arrowTarget.classList.remove(this.expandedClass)
+      this.arrowTarget.classList.add(this.collapsedClass)
+      leave(this.menuTarget)
+    }
+  }
+
+  toggle(event) {
+    this.openValue = !this.openValue
+  }
+}
