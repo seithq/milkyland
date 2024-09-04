7c5109b895620fd09c31f0d315f0fd69a31e305e
danikarik <daniyar@hey.com>
2024-09-04 14:30:55 +0500

    WIP: plans view

--- app/controllers/sales/plans_controller.rb
+++ app/controllers/sales/plans_controller.rb
@@ -1,8 +1,9 @@
 class Sales::PlansController < ApplicationController
+  before_action :set_status
   before_action :set_plan, only: %i[ show edit update destroy ]
 
   def index
-    @plans = Plan.all
+    @pagy, @plans = pagy get_scope(params)
   end
 
   def show
@@ -12,29 +13,37 @@ class Sales::PlansController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @plan.update(plan_params)
-        format.html { redirect_to plan_url(@plan), notice: "Plan was successfully updated." }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
+    if @plan.update(plan_params)
+      redirect_on_update plans_url(status: @plan.status)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @plan.cancel
 
-    respond_to do |format|
-      format.html { redirect_to plans_url, notice: "Plan was successfully destroyed." }
-    end
+    redirect_on_destroy plan_url, text: t("actions.plan_cancelled")
   end
 
   private
+    def base_scope
+      Plan.filter_by_status(@status).ordered
+    end
+
+    def search_methods
+      []
+    end
+
     def set_plan
       @plan = Plan.find(params[:id])
     end
 
     def plan_params
-      params.require(:plan).permit(:production_date, :status)
+      params.require(:plan).permit(:status)
+    end
+
+    def set_status
+      @status = params[:status].presence || "in_consolidation"
     end
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,10 +2,14 @@ class Plan < ApplicationRecord
   has_many :consolidations, dependent: :destroy
   has_many :orders, through: :consolidations
   has_many :positions, through: :orders
+  has_many :products, through: :positions
+  has_many :groups, through: :products
 
   validates :production_date, presence: true, comparison: { greater_than_or_equal_to: Time.zone.today }
 
   enum :status, %w[ in_consolidation in_production completed cancelled ].index_by(&:itself), default: :in_consolidation
+
+  scope :filter_by_status, ->(status) { where status: status }
 
   scope :ordered, -> { order(production_date: :desc) }
 
@@ -14,10 +18,15 @@ class Plan < ApplicationRecord
   end
 
   def cancel
+    consolidations.update_all active: false
     update! status: :cancelled
   end
 
   def add(order)
     self.consolidations.create! order: order
+  end
+
+  def can_edit?
+    in_consolidation?
   end
 end
