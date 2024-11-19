9e5ae98de36dd98eb0591e5bebd844f8ae21514b
Daniyar Baitursynov <daniyar@hey.com>
2024-11-20 00:03:15 +0500

    Add base mobile waybills controller

---
+++ app/controllers/mobile/base_waybills_controller.rb
@@ -0,0 +1,69 @@
+module Mobile
+  class BaseWaybillsController < MobileController
+    before_action :set_waybill, only: %i[ show edit update destroy ]
+
+    def show
+    end
+
+    def new
+      @waybill = base_scope.new(kind: base_kind)
+    end
+
+    def edit
+    end
+
+    def create
+      @waybill = base_scope.new(waybill_params)
+
+      if @waybill.save
+        redirect_on_create waybill_edit_url(@waybill)
+      else
+        render_with_error :new, @waybill
+      end
+    end
+
+    def update
+      if @waybill.update(waybill_params)
+        trigger_processing_job(@waybill) if @waybill.approved?
+        redirect_on_update waybill_show_url(@waybill)
+      else
+        render_with_error :edit, @waybill
+      end
+    end
+
+    def destroy
+      @waybill.destroy!
+
+      redirect_on_destroy waybill_journal_url
+    end
+
+    private
+      def waybill_edit_url(waybill)
+        raise NotImplementedError.new "Implement in subclass"
+      end
+
+      def waybill_show_url(waybill)
+        raise NotImplementedError.new "Implement in subclass"
+      end
+
+      def waybill_journal_url
+        raise NotImplementedError.new "Implement in subclass"
+      end
+
+      def base_kind
+        raise NotImplementedError.new "Implement in subclass"
+      end
+
+      def trigger_processing_job(waybill)
+        raise NotImplementedError.new "Implement in subclass"
+      end
+
+      def set_waybill
+        @waybill = base_scope.find(params.expect(:id))
+      end
+
+      def waybill_params
+        params.expect(waybill: [ :kind, :storage_id, :new_storage_id, :sender_id, :receiver_id, :status ]).with_defaults(kind: base_kind)
+      end
+  end
+end
--- app/controllers/mobile/waybills/arrivals_controller.rb
+++ app/controllers/mobile/waybills/arrivals_controller.rb
@@ -1,53 +1,29 @@
 module Mobile
-  class Waybills::ArrivalsController < MobileController
-    before_action :set_waybill, only: %i[ show edit update destroy ]
-
-    def show
-    end
-
-    def new
-      @waybill = base_scope.new(kind: :arrival)
-    end
-
-    def edit
-    end
-
-    def create
-      @waybill = base_scope.new(waybill_params)
-
-      if @waybill.save
-        redirect_on_create edit_waybills_arrival_url(@waybill)
-      else
-        render_with_error :new, @waybill
-      end
-    end
-
-    def update
-      if @waybill.update(waybill_params)
-        ProcessArrivalCodesJob.perform_later @waybill.id if @waybill.approved?
-        redirect_on_update waybills_arrival_url(@waybill)
-      else
-        render_with_error :edit, @waybill
-      end
-    end
-
-    def destroy
-      @waybill.destroy!
-
-      redirect_on_destroy journals_outgoings_url
-    end
-
+  class Waybills::ArrivalsController < BaseWaybillsController
     private
       def base_scope
         Current.user.out_waybills
       end
 
-      def set_waybill
-        @waybill = base_scope.find(params.expect(:id))
+      def waybill_edit_url(waybill)
+        edit_waybills_arrival_url(waybill)
       end
 
-      def waybill_params
-        params.expect(waybill: [ :kind, :storage_id, :new_storage_id, :sender_id, :receiver_id, :status ]).with_defaults(kind: :arrival)
+      def waybill_show_url(waybill)
+        waybills_arrival_url(waybill)
+      end
+
+      def waybill_journal_url
+        journals_outgoings_url
+      end
+
+      def base_kind
+        :arrival
+      end
+
+      def trigger_processing_job(waybill)
+        return unless waybill.approved?
+        ProcessArrivalCodesJob.perform_later waybill.id
       end
   end
 end
