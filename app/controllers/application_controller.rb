d91bc71df4368082e8297dfaba72e7bcd5a60efd
Daniyar Baitursynov <daniyar@hey.com>
2024-11-23 21:40:27 +0500

    Upgrade Rails 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   include Authentication, Authorization, PageLayouts, Searchable, VersionHeaders, Flashable
-  include Pagy::Backend, Zipline
+  include Pagy::Backend
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: { safari: 16.4, chrome: 120, firefox: 121, opera: 106, ie: false }
